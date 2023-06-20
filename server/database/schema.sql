971600797aacd9832243dcc45c5f7a8c80f44d8a
Ali Thursland <alexandra.thursland@duke.edu>
2023-06-19 20:34:57 -0400

    updated database schema to include jobs

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -8,6 +8,23 @@ CREATE TABLE users (
     major TEXT NOT NULL
 );
 
+CREATE TABLE jobs (
+    id integer PRIMARY key AUTO_INCREMENT,
+    company VARCHAR(40) NOT NULL,
+    title VARCHAR(40) NOT NULL,
+    progress ENUM('not started', 'started', 'completed') NOT NULL,
+    response CASE WHEN progress = 'completed' ENUM('pending', 'rejected', 'offer received'),
+    deadline DATE NOT NULL,
+    details TEXT, 
+    recruiter_name VARCHAR(100),
+    recruiter_phone VARCHAR(15),
+    recruiter_email VARCHAR(100)
+)
+
+INSERT INTO jobs (company, title, progress, deadline)
+VALUES
+('spotify', 'data scientist', 'not started', '2023-07-01')
+
 INSERT INTO users (title, email, major)
 VALUES
 ('carola', 'carolamaglione@gmail.com', 'computer science');
